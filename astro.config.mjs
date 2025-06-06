d6614af4ad29775a3acbd8e8c3023164b9512154
awfixer <git@awfixer.me>
2025-06-05 20:03:24 -0700

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,9 @@ export default defineConfig({
     "/github": "https://github.com/awfixer-org",
     "/terms": "https://legal.awfixer.com/terms",
     "/privacy": "https://legal.awfixer.com/privacy",
+    "/gitlab": "https://gitlab.awfixer.dev",
+    "/dev": "https://awfixer.dev",
+    "/cloud": "https://awfixer.cloud",
+    "/blog": "https://awfixer.blog",
   },
 });
