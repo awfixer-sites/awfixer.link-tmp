ab0c25cce1574a1dc4f60969b84ca06d234db8de
awfixer <git@awfixer.me>
2025-06-11 00:39:55 -0700

    update README.md

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,5 +14,6 @@ export default defineConfig({
     "/dev": "https://awfixer.dev",
     "/cloud": "https://awfixer.cloud",
     "/blog": "https://awfixer.blog",
+    "/cal": "https://cal.com/awfixer",
   },
 });
